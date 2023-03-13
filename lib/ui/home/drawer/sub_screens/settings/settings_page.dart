5582a419e8e6da50ca00960ebc026a36cdbe1755
Saidazam <saidolmovsaid@gmail.com>
2023-03-13 08:14:49 -0700

    settings

--- lib/ui/home/drawer/sub_screens/settings/settings_page.dart
+++ lib/ui/home/drawer/sub_screens/settings/settings_page.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -11,6 +10,6 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
